c400c674e6c6e6059f1d89fef6a930991c5ffede
nvakiet <63222542+nvakiet@users.noreply.github.com>
2022-03-28 11:49:57 +0700

    Changed ClickQueryDemo to use a task scheduler

--- ClickQueryDemo.py
+++ ClickQueryDemo.py
@@ -1,11 +1,21 @@
 # import modules
 from pyspark.sql import SparkSession
 from redis import Redis
+import sched, time
 
 # set the app name and Redis address
 APPNAME = "ClickQuery"
 RHOST = "127.0.0.1"
 RPORT = "6379"
+
+def doQuery(df, scheduler):
+    try:
+        # Call the select query again whenever needed, it will sync with Redis data
+        df.select("*").show()
+        # Call a new scheduler to repeat this function
+        scheduler.enter(1, 1, doQuery, (df, scheduler))
+    except Exception as e:
+        print(e)
 
 def main():
     try:
@@ -14,7 +24,7 @@ def main():
             .appName(APPNAME)\
             .master("local[*]")\
             .getOrCreate()
-
+        spark.sparkContext.setLogLevel("WARN")
         # Connect to Redis
         r = Redis(RHOST, RPORT)
 
@@ -25,9 +35,9 @@ def main():
         df = spark.read.format("org.apache.spark.sql.redis").schema("asset STRING, count INT").option("table", "click").load()
         
         # should use an event scheduler library to run the content of the loop every second instead of this
-        for i in range(1000): 
-            df.select("*").show() # Call the select query again whenever needed, it will sync with Redis data
-            
+        s = sched.scheduler(time.time, time.sleep)
+        s.enter(1, 1, doQuery, (df,s))
+        s.run(blocking=True) # set blocking=false when using this in a server
         # end program
         r.close()
         spark.stop()
